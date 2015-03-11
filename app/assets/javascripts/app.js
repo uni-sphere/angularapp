da53ef26cb0fb761ec831520fe3e4db632ec2d8c
Gabriel <gabriel.muller.12@gmail.com>
2015-03-11 20:48:30 +0100

    JSON get ok

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,18 +1,20 @@
 (function(){
-  angular.module('myApp', ['ngAnimate','ui.router','templates']).config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  angular
+  .module('myApp', ['ngAnimate','ui.router','templates','ngResource'])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
    
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'home.html',
-        controller: 'HomeCtrl',
-        resolve: {
-          chapters: ['$http', function($http){
-            return $http.get('api/chapters.json').then(function(response){
-              return response.data;
-            })
-          }]
-        }
+        controller: 'HomeCtrl'
+        // resolve: {
+        //   chapters: ['Chapters', function(Chapters){
+        //     return Chapters.get(function(){
+        //       console.log(Chapters.get());
+        //     });
+        //   }]
+        // }
       })
       
      .state('dashboard', {
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'angular#index'
 
-  get "*path.html", to: "angular#index", layout: 0
-  get "*path", to: "angular#index"
+  # get "*path.html", to: "angular#index", layout: 0
+  # get "*path", to: "angular#index"
 
   get "/api/chapters", to: "angular#show"
 
