e8aeacb6122a50fe2b41ed401212a3b5ab61de16
ClemPops <clement_muller@hotmail.fr>
2015-04-08 16:02:27 +0200

    temp

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,8 @@ module Uni2
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    
+    config.encoding = "utf-8"
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
