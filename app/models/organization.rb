dc908250ef6fe3525c59b432afb9cd999a569347
ClemPops <clement_muller@hotmail.fr>
2015-05-13 13:14:31 +0200

    temp

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -12,7 +12,7 @@ class Organization < ActiveRecord::Base
   
   def format_subdomain
     if self.website.include? 'www'
-      self.subdomain = self.website.split('/')[2].split('.').first
+      self.subdomain = self.website.split('/')[2].split('.')[1]
     else
       self.subdomain = self.website.split('.')[1]
     end
