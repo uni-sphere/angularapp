de1d0495867918796059deebe806ad010c0ee114
Gabriel <gabriel.muller.12@gmail.com>
2015-05-15 12:37:56 +0200

    mobile-view

--- app/controllers/angular_controller.rb
+++ app/controllers/angular_controller.rb
@@ -1,11 +1,13 @@
 class AngularController < ApplicationController
 
   layout 'main'
-  
+
   def index
-    render 'mobile/main' if is_mobile?
+    if is_mobile?
+      render :layout => 'mobile'
+    end
   end
-  
+
   def is_mobile?
     (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/) ? true : false
   end
