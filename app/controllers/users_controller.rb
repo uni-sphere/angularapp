430a9582cfd8bc0f37e143f19f8e9a05fcc9a24f
ClemPops <clement_muller@hotmail.fr>
2015-05-16 13:26:08 +0200

    temp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
     if params[:emails]
       emails = params[:emails]
       emails.each do |email|
-        send_error('user not created', 500) unless User.invite!(email: email, name: email.split('@').first, provider: 'email', organization_id: current_organization.id)
+        psw = random_password
+        if user = User.invite!(email: email, name: email.split('@').first, provider: 'email', organization_id: current_organization.id, skip_invitation: true)
+          user.deliver_invitation
+          UserMailer.invite_user_email(email: email, name: email.split('@').first, password: psw).deliver
+        else
+          send_error('user not created', 500) 
+        end
       end
       render json: {response: 'success'}, status: 200
     else
