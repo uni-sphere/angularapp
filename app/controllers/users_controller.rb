aba1632cdfe44f13891aae30529159c17e14368d
ClemPops <clement_muller@hotmail.fr>
2015-05-16 13:30:46 +0200

    temp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
         psw = random_password
         if user = User.invite!(email: email, name: email.split('@').first, provider: 'email', organization_id: current_organization.id, skip_invitation: true)
           user.deliver_invitation
-          UserMailer.invite_user_email(email: email, name: email.split('@').first, password: psw).deliver
+          UserMailer.invite_user_email(email, current_organization, psw).deliver
         else
           send_error('user not created', 500) 
         end
