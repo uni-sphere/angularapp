b387dde67827d116e35412876868b91c256232b9
ClemPops <clement_muller@hotmail.fr>
2015-06-12 14:46:40 +0200

    temp

--- db/migrate/20150307080000_create_organizations.rb
+++ db/migrate/20150307080000_create_organizations.rb
@@ -3,8 +3,8 @@ class CreateOrganizations < ActiveRecord::Migration
     create_table :organizations do |t|
       t.string :name
       t.string :subdomain
-      t.decimal :latitude
-			t.decimal :longitude
+      t.string :latitude
+			t.string :longitude
       t.string :place_id
       t.string :website
       
