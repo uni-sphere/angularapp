5c8db2250668c28744718af69936b28a66da3ae1
Gabriel <gabriel.muller.12@gmail.com>
2015-06-13 15:06:14 +0200

    error display fixed

--- app/assets/javascripts/directives/errorDisplay.js
+++ app/assets/javascripts/directives/errorDisplay.js
@@ -6,13 +6,11 @@
         restrict: 'E',
         templateUrl: 'main/error-display.html',
         scope: {
+          hideError: '=?bind',
           listError: '=',
-          showError: '=',
-          hideError: '='
+          showError: '='
         },
         link: function(scope) {
-          scope.listError = []
-          scope.showError = false
           scope.hideError = function(){
             scope.listError = [];
             scope.showError = false;
