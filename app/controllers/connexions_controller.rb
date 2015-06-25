e14ccdcfa1b712a074790a35b0414a7f331aa815
ClemPops <clement_muller@hotmail.fr>
2015-06-25 22:25:40 +0200

    dsc order top users

--- app/controllers/connexions_controller.rb
+++ app/controllers/connexions_controller.rb
@@ -4,7 +4,7 @@ class ConnexionsController < ApplicationController
     activity = {}
     Organization.all.each do |organization|
       active_connexions = organization.connexions.where("updated_at >= ?", Time.now - 2.minutes)
-      top_co = organization.connexions.order(:count).limit(5)
+      top_co = organization.connexions.order(count: :desc).limit(5)
       awsdocuments = organization.awsdocuments.count
       users = organization.users.count
       activity[organization.subdomain] = [active_connexions.count, awsdocuments, users, top_co]
