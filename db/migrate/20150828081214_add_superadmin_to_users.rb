87a272ecc78307ff9f4087f6065c891f9907d9fe
ClemPops <clement_muller@hotmail.fr>
2015-08-28 19:40:56 +0200

    t

--- db/migrate/20150828081214_add_superadmin_to_users.rb
+++ db/migrate/20150828081214_add_superadmin_to_users.rb
@@ -1,5 +1,5 @@
 class AddSuperadminToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :archived, :superadmin, default: false
+    add_column :users, :archived, :super_admini, default: false
   end
 end
