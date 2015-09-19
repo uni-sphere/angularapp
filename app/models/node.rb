abc2d91377ef6408c561ab52c358e696b7857a51
Gabriel <gabriel.muller.12@gmail.com>
2015-09-19 15:43:11 +0200

    sfsdf

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,7 +1,7 @@
 class Node < ActiveRecord::Base
 
   include BCrypt
-  
+
   belongs_to :organization
   belongs_to :user
 
@@ -9,16 +9,16 @@ class Node < ActiveRecord::Base
   has_many :reports
 
   validates :name, :parent_id, :user_id, presence: true
-  
+
   before_save :set_superadmin
-  
+
   def set_superadmin
     self.superadmin = true if User.find(self.user_id).superadmin
   end
-  
+
   def self.create_reports
     self.where(archived: false).each do |node|
-      node.reports.create if node.chapters.where(archived: false).count > 0 and Time.now - node.reports.last.created_at >= 7.days 
+      node.reports.create if node.chapters.where(archived: false).count > 0 and Time.now - node.reports.last.created_at >= 7.days
     end
   end
 
