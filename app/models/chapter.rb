2daa0685625c5f1a9c53a704e747e3b80c804267
ClemPops <clement_muller@hotmail.fr>
2015-11-25 09:04:34 +0100

    t

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -18,8 +18,12 @@ class Chapter < ActiveRecord::Base
   def add_position
     if !self.position 
       brothers = Chapter.where(node_id: self.node_id, archived: false, parent_id: self.parent_id)
-      if brothers.count == 0
+      if self.parent_id == 0
+        last_position = -1
+      elsif brothers.count == 0 and Chapter.find(self.parent_id).parent_id == 0
         last_position = 0
+      elsif brothers.count == 0
+        last_position = 0  
       else
         last_position = brothers.order('position DESC').first.position
       end
